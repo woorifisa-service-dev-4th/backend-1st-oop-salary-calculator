e10eabf5b7bcab1adf4bc23bfec9aac1573a72cd
june0216 <ziyun1612@ewhain.net>
2025-02-11 15:09:48 +0900

    fix : 범위 예외 제한 추가

--- src/main/java/cal/sal/utils/InputUtils.java
+++ src/main/java/cal/sal/utils/InputUtils.java
@@ -14,7 +14,7 @@ public class InputUtils {
 
 				return scanner.nextBigDecimal();
 			} catch (InputMismatchException e) {
-				System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+				System.out.println("잘못된 입력입니다 ㅜ^ㅜ  숫자를 입력해주세요.");
 				scanner.next(); // 잘못된 입력값 제거
 			}
 		}
@@ -27,7 +27,7 @@ public class InputUtils {
 
 				return scanner.nextDouble();
 			} catch (InputMismatchException e) {
-				System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+				System.out.println("잘못된 입력입니다 ㅜ^ㅜ  숫자를 입력해주세요.");
 				scanner.next(); // 잘못된 입력값 제거
 			}
 		}
@@ -40,7 +40,7 @@ public class InputUtils {
 
 				return scanner.nextInt();
 			} catch (InputMismatchException e) {
-				System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
+				System.out.println("잘못된 입력입니다 ㅜ^ㅜ  정수를 입력해주세요.");
 				scanner.next(); // 잘못된 입력값 제거
 			}
 		}
@@ -49,7 +49,8 @@ public class InputUtils {
 	public static BigDecimal InputHourlyWage() {
 		BigDecimal hourlyWage = InputBigDecimal();
 		if (hourlyWage.compareTo(BigDecimal.ZERO) < 0) {
-			System.out.println("잘못된 입력입니다. 양수로 다시 입력해주세요.");
+			System.out.println("잘못된 입력입니다 ㅜ^ㅜ  시급은 음수가 될 수 없습니다.");
+			System.out.println("양수로 다시 입력해주세요.");
 			InputBigDecimal();
 		}
 		return hourlyWage;
@@ -57,8 +58,9 @@ public class InputUtils {
 
 	public static double InputDailyHours() {
 		double dailyWorkHours = InputDouble();
-		if (dailyWorkHours < 0) {
-			System.out.println("잘못된 입력입니다. 양수로 다시 입력해주세요.");
+		if (dailyWorkHours <= 0) {
+			System.out.println("잘못된 입력입니다 ㅜ^ㅜ 일한 시간이 0이거나 음수이면 계산할 수 없습니다. ");
+			System.out.println("양수로 다시 입력해주세요.");
 			InputDouble();
 		}
 		return dailyWorkHours;
@@ -66,8 +68,9 @@ public class InputUtils {
 
 	public static int InputWeeklyWorkDays() {
 		int weeklyWorkDays = InputInt();
-		if (weeklyWorkDays > 7 || weeklyWorkDays < 0) {
-			System.out.println("잘못된 입력입니다 0 ~ 7까지 중 하나로 다시 작성해주세요.");
+		if (weeklyWorkDays > 7 || weeklyWorkDays <= 0) {
+			System.out.println("잘못된 입력입니다 ㅜ^ㅜ  0시간 이하이면 계산할 수 없습니다. ");
+			System.out.println("1 ~ 7까지 중 하나로 다시 작성해주세요.");
 			InputInt(); // 잘못된 입력값 제거
 		}
 		return weeklyWorkDays;
@@ -75,6 +78,11 @@ public class InputUtils {
 
 	public static double InputMonthlyOvertimeHours() {
 		double monthlyOvertimeHours = InputDouble();
+		if(monthlyOvertimeHours < 0) {
+			System.out.println("잘못된 입력입니다 ㅜ^ㅜ  초과 근무시간은 음수가 될 수 없습니다.");
+			System.out.println("양수로 다시 입력해주세요.");
+			InputDouble();
+		}
 		return monthlyOvertimeHours;
 	}
 
