1ebf4bf5439ecd6e77245cd0634aa05dabbcfaf0
HyeYoung-Hwang <vkfvks1004@naver.com>
2025-02-11 19:28:04 +0900

    근무 시간 출력 리팩토링

--- src/main/java/cal/sal/Main.java
+++ src/main/java/cal/sal/Main.java
@@ -7,6 +7,7 @@ import cal.sal.userInput.UserInput;
 import cal.sal.utils.PrintUtils;
 import cal.sal.workschedule.MonthlyWorkSchedule;
 import cal.sal.workschedule.WeeklyWorkSchedule;
+import cal.sal.workschedule.WorkSchedulePrint;
 
 public class Main {
 	public static void main(String[] args) {
@@ -18,15 +19,13 @@ public class Main {
 		WeeklyWorkSchedule weeklyWorkSchedule = new WeeklyWorkSchedule();
 		double weeklyHours = weeklyWorkSchedule.calculateWorkHours(userInput);
 
-		System.out.println("======= 입력하신 정보를 기반으로 계산을 해보았습니다! ^^ =======");
-		System.out.println("총 주간 근무 시간: " + weeklyHours + " 시간"); // TODO 리팩터링 예정
-
 		MonthlyWorkSchedule monthlyWorkSchedule = new MonthlyWorkSchedule();
 		double monthlyHours = monthlyWorkSchedule.calculateWorkHours(userInput);
 
-		System.out.println("총 월간 근무 시간: " + monthlyHours + " 시간");
-
-		System.out.println("======================================================");
+		WorkSchedulePrint.printCalculationResult();
+		WorkSchedulePrint.printTotalWeeklyHours(weeklyHours);
+		WorkSchedulePrint.printTotalMonthlyHours(monthlyHours);
+		WorkSchedulePrint.printSeparatorLine();
 
 		SalaryResult salaryResult = SalaryCalculatorUtil.calculateSalaries(
 			userInput);
